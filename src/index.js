ea96af07b460d44d0b55e31747c0e2ccd61d23cd
Marcelo Barbosa <marceloribeirobarbosa92@gmail.com>
2025-02-23 17:53:09 -0300

    Componetização App

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import { createRoot } from "react-dom/client";
-import NavBar from './NavBar';
-import Body from "./Body";
-import Stories from "./Stories";
-import Posts from "./Posts";
-
-
-function App(){
-    return (
-<>
-   <NavBar />
-   <Body/>
-
-</>     
-    )
-}
+import App from "./App";
 
 
 
