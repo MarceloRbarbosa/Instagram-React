5e6cbcac3322f99fb0fec1fa371812b7aa1751e7
Marcelo Barbosa <marceloribeirobarbosa92@gmail.com>
2025-02-25 00:25:09 -0300

    Atualização troca Avatar do perfil User

--- src/User.js
+++ src/User.js
@@ -1,11 +1,16 @@
 import React from "react";
+const imagemUsuario = '../assets/img/catanacomics.svg' ;
+
+
 
 export default function User(){
     const [nome, setNome] = React.useState('Solove');
+    const [fotoPerfil, setFotoPerfil] = React.useState(imagemUsuario);
     return (
     <>
+    
     <div className="usuario">        
-            <img src='../assets/img/catanacomics.svg' />       
+            <img  onClick={alterarImagem} src={fotoPerfil} />       
             <div className="texto">
             <strong>{nome}</strong>  
             <ion-icon onClick={trocarApelido} name="pencil-sharp"></ion-icon>
@@ -18,5 +23,10 @@ export default function User(){
 function trocarApelido(){
 const novoNome = prompt('Alterar apelido');
 (novoNome === '') ? alert('Necessita de um apelido valido') : setNome(novoNome) ;
+}
+
+function alterarImagem(){
+const novaImagem = prompt('Insira aqui seu link para imagem');
+(novaImagem === '') ? alert('Necessita de um Link valido'): setFotoPerfil(novaImagem);
 }
 }
